205b24d1f2c4f593ff12750bbe2b2dcaf4a086b5
Ann <wangyuwei@upg.cn>
2018-12-06 21:49:20 +0800

    fixed

---
+++ src/main/java/per/design/patterns/command/advanced/CeilingFan.java
@@ -0,0 +1,38 @@
+package per.design.patterns.command.advanced;
+
+/**
+ * 接受者(receiver): 电风扇
+ */
+public class CeilingFan {
+
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
+    public String location;
+    public int speed;
+
+    public CeilingFan() {
+        speed = OFF;
+    }
+
+    public void high() {
+        speed = HIGH;
+    }
+
+    public void medium() {
+        speed = MEDIUM;
+    }
+
+    public void low() {
+        speed = LOW;
+    }
+
+    public void off() {
+        speed = OFF;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
--- src/main/java/per/design/patterns/command/advanced/InvokerControl.java
+++ src/main/java/per/design/patterns/command/advanced/InvokerControl.java
@@ -6,6 +6,7 @@ public class InvokerControl {
     public void run() {
         Invoker invoker = new Invoker();
 
+        /* 开关灯、撤销操作 */
         int index = 0;
         Light bedroomLight = new BedRoomLight();
         Command bedroomCommandOn = new LightOnCommand(bedroomLight);
@@ -24,6 +25,16 @@ public class InvokerControl {
 
         invoker.buttonPushedOn(1);
         invoker.buttonPushedOn(1);
+
+
+        /* 开不同档风扇、撤销操作 */
+        CeilingFan ceilingFan = new CeilingFan();
+        Command ceilingFanOn = new CeilingFanMediumOnCommand(ceilingFan);
+        Command ceilingFanOff = new CeilingFanMediumOffCommand(ceilingFan);
+        invoker.setCommand(0, ceilingFanOn, ceilingFanOff);
+        invoker.buttonPushedOn(0);
+        invoker.buttonPushedOff(0);
+        invoker.buttonPushedUndo();
     }
 
 }
